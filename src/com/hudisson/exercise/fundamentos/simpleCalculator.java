1325a06c1ea2a2f49c1b89b0d90166c3c9922a26
Hudisson R Xavier <hudissonxavier2018@gmail.com>
2026-03-06 23:13:05 -0300

    Exercício 01 - Calculadora simples

---
+++ src/com/hudisson/exercise/fundamentos/simpleCalculator.java
@@ -0,0 +1,75 @@
+/**
+ * @author Hudisson Xavier
+ * 
+ * Execício 01: 
+ *      Crie um programa que receba dois números e uma operação (+ - * /) e retorne o resultado.
+ */
+package com.hudisson.exercise.fundamentos;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class simpleCalculator {
+ 
+    public static double Addition(double a, double b){
+        return a + b;
+    }
+
+    public static double Subtraction(double a, double b){
+        return a - b;
+    }
+
+    public static double Multiplication(double a, double b){
+        return a * b;
+    }
+
+    public static double Division(double a, double b){
+        return a/b;
+    }
+
+    public static void main(String[] args){
+
+        System.out.println("=======================Simple Calculator=================================");
+
+        Scanner scan = new Scanner(System.in);
+
+        double valor_a, valor_b;
+
+        System.out.println("Digite o primeiro valor: ");
+        valor_a = scan.nextDouble();
+
+        System.out.println("Digite o segundo valor: ");
+        valor_b = scan.nextDouble();
+
+        System.out.println("Escolha uma operação: (+, -, *, /)");
+        String operacao = scan.next();
+
+        DecimalFormat df = new DecimalFormat("#.00");
+        
+        switch (operacao) {
+            case "+":
+               System.out.println("Resultado: "+ df.format(Addition(valor_a, valor_b)));
+               break;
+            case "-":
+                
+                System.out.println("Resultado: "+ df.format(Subtraction(valor_a, valor_b)));
+                break;
+            
+            case "*":
+                System.out.println("Resultado: "+ df.format(Multiplication(valor_a, valor_b)));
+                break;
+
+            case "/":
+                System.out.println("Resultado: "+ df.format(Division(valor_a, valor_b)));
+                break;
+
+            default:
+                System.out.println("Operação inválida");
+                break;
+        
+        }
+
+        System.out.println("=============================================================");
+
+    }
+}
