06bbe517c14324160321adea9bdca53d8ea6085c
Hudisson R Xavier <hudissonxavier2018@gmail.com>
2026-03-17 17:25:28 -0300

    Exercício 06 - Contar vogais em um texto e retorna as vogais

--- src/com/hudisson/exercise/fundamentos/contarVogais.java
+++ src/com/hudisson/exercise/fundamentos/contarVogais.java
@@ -28,14 +28,35 @@ public class contarVogais {
         return conatador;
     }
 
+    // método que retorna as voagais do texto
+    public static String vowelsText(String texto){
+        String[] letras = texto.toLowerCase().split("");
+        StringBuilder vogais = new StringBuilder();
+
+        for(int i = 0; i <= (letras.length -1); i++){
+            String ch = letras[i];
+
+          if(ch.equals("a") || ch.equals("e") || ch.equals("i") || ch.equals("o") || ch.equals("u")){
+                if(vogais.length() > 0){
+                    vogais.append("; ");
+                }
+                vogais.append(letras[i]);
+                
+          }
+        }
+
+        return vogais.toString();
+    }
+
     public static void main(String[] args){
 
-        String txt = "Java"; // sáida 2
+        String txt = "Casinha";
 
         System.out.println("\n-----Contado vogais em um texto-----");
 
         System.out.println("\nTexto: "+ txt);
         System.out.println("Quantidade de vogais: "+countVowels(txt));
+        System.out.println("Vogais: "+vowelsText(txt));
         System.out.println("\n------------------------------------------------\n");
     }
 
